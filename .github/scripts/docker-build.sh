722ce1632dc058748fa381cc76e08fec899836dc
jakethecoder95 <95jacob07@gmail.com>
2024-03-21 16:48:42 -0700

    Perform Deployment

--- .github/scripts/docker-build.sh
+++ .github/scripts/docker-build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 docker build \
-  --tag "$GAR_ZONE-docker.pkg.dev/$PROJECT_ID/$GAR_REPO/$IMAGE:$IMAGE_TAG" \
+  --tag "$GAR_ZONE-docker.pkg.dev/$PROJECT_ID/$GAR_REPO/$IMAGE:latest" \
   --tag "$GAR_ZONE-docker.pkg.dev/$PROJECT_ID/$GAR_REPO/$IMAGE:$GITHUB_SHA" \
   ./app
 
-docker push "$GAR_ZONE-docker.pkg.dev/$PROJECT_ID/$GAR_REPO/$IMAGE:$IMAGE_TAG"
+docker push "$GAR_ZONE-docker.pkg.dev/$PROJECT_ID/$GAR_REPO/$IMAGE:latest"
 docker push "$GAR_ZONE-docker.pkg.dev/$PROJECT_ID/$GAR_REPO/$IMAGE:$GITHUB_SHA"
+
+kubectl apply -f k8s
+kubectl set image deployments/devguy-deployment devguy=$IMAGE:$GITHUB_SHA
