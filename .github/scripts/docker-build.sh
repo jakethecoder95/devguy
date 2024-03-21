4d094fb18b3bee910493657f0b54382ae298b66e
jakethecoder95 <95jacob07@gmail.com>
2024-03-21 15:49:29 -0700

    Fix App Location

--- .github/scripts/docker-build.sh
+++ .github/scripts/docker-build.sh
@@ -2,6 +2,6 @@
 docker build \
   --tag "$GAR_ZONE-docker.pkg.dev/$PROJECT_ID/$GAR_REPO/$IMAGE:$IMAGE_TAG" \
   --tag "$GAR_ZONE-docker.pkg.dev/$PROJECT_ID/$GAR_REPO/$IMAGE:$GITHUB_SHA" \
-  ../app
+  ../../app
 
 docker push "$GAR_ZONE-docker.pkg.dev/$PROJECT_ID/$GAR_REPO/$IMAGE:$IMAGE_TAG"
