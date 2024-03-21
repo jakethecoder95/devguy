1b6cab21e01b5bbb3cc6b5c6824e6a3654f5c825
jakethecoder95 <95jacob07@gmail.com>
2024-03-21 15:28:49 -0700

    Add Bash

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker build \
   --tag "$GAR_ZONE-docker.pkg.dev/$PROJECT_ID/$GAR_REPO/$IMAGE:$IMAGE_TAG" \
   --tag "$GAR_ZONE-docker.pkg.dev/$PROJECT_ID/$GAR_REPO/$IMAGE:$GITHUB_SHA" \
