1441bca9e0f8a8a144bd912108db039313a38f5d
jakethecoder95 <95jacob07@gmail.com>
2024-03-21 15:21:31 -0700

    Simplify Docker Step

---
+++ scrips/docker-build.sh
@@ -0,0 +1,6 @@
+docker build \
+  --tag "$GAR_ZONE-docker.pkg.dev/$PROJECT_ID/$GAR_REPO/$IMAGE:$IMAGE_TAG" \
+  --tag "$GAR_ZONE-docker.pkg.dev/$PROJECT_ID/$GAR_REPO/$IMAGE:$GITHUB_SHA" \
+  ../app
+
+docker push "$GAR_ZONE-docker.pkg.dev/$PROJECT_ID/$GAR_REPO/$IMAGE:$IMAGE_TAG"
