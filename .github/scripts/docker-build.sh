093aead6232e99bce8afbb3424f4532f5c182e57
jakethecoder95 <95jacob07@gmail.com>
2024-03-22 08:48:33 -0700

    Simplify Docker Script

--- .github/scripts/docker-build.sh
+++ .github/scripts/docker-build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 docker buildx build \
   --platform "linux/amd64" \
-  --tag "$DOCKERHUB_USERNAME/$IMAGE:latest" \
-  --tag "$DOCKERHUB_USERNAME/$IMAGE:$GITHUB_SHA" \
+  --tag "jakethecoder95/devguy:latest" \
+  --tag "jakethecoder95/devguy:$GITHUB_SHA" \
   ./app
 
-docker push "$DOCKERHUB_USERNAME/$IMAGE:latest"
-docker push "$DOCKERHUB_USERNAME/$IMAGE:$GITHUB_SHA"
+docker push "jakethecoder95/devguy:latest"
+docker push "jakethecoder95/devguy:$GITHUB_SHA"
 
 kubectl apply -f k8s
-kubectl set image deployments/devguy-deployment devguy=$IMAGE:$GITHUB_SHA
+kubectl set image deployments/devguy-deployment devguy=jakethecoder95/devguy:$GITHUB_SHA
