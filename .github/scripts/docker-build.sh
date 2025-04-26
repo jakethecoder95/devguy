3133da6f05ab46b8f79a3f083c0586c53b23fbe0
jakethecoder95 <95jacob07@gmail.com>
2025-04-25 22:26:48 -0700

    Fixup

--- .github/scripts/docker-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker buildx build \
-  --platform "linux/amd64" \
-  --tag "jakethecoder95/devguy:latest" \
-  --tag "jakethecoder95/devguy:$GITHUB_SHA" \
-  ./app
-
-docker push "jakethecoder95/devguy:latest"
-docker push "jakethecoder95/devguy:$GITHUB_SHA"
